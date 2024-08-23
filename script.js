98f027b215f335090b5f23f1c2a01b6cde36fe12
joris <97834949+Georgyporgie@users.noreply.github.com>
2024-08-23 18:36:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleDarkMode() {
+var el = document.querySelector(
+  "body");
+el.classList.toggle("darkMode")
+}
+
+function galleryOn() {
+var el = document.querySelector(
+  "div");
+el.classList.add("border",
+  "bg-white");
+}
